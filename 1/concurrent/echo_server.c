486bea9101ab3baa0e96790c6aab4e7a172c9ac3
Gracious Saxena <43841726+GraciousSaxena@users.noreply.github.com>
2021-01-13 00:12:12 +0530

    Update echo_server.c

--- 1/concurrent/echo_server.c
+++ 1/concurrent/echo_server.c
@@ -1,63 +1,75 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <arpa/inet.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<sys/stat.h>
 
-void str_echo(int connfd, int port) {
-    int n;
-    int bufsize = 1024;
-    char *buffer = malloc(bufsize);
-again:
-    while ((n = recv(connfd, buffer, bufsize, 0)) > 0) {
-        send(connfd, buffer, n, 0);
-        fputs(buffer, stdout);
-        printf("Port:%d \n", port);
-    }
-    if (n < 0)
-        goto again;
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+#include<netinet/in.h>
+#include<arpa/inet.h>
+
+void str_echo(int connfd){
+	int n,bufsize = 1024,len;
+	char *buff = malloc(bufsize);
+	struct sockaddr_in addr;
+
+again: while((n=recv(connfd,buff,bufsize,0))>0){
+	       getpeername(connfd,(struct sockaddr *) &addr,&len);
+	       printf("The client with %s ip and port %d sent - \n %s \n",inet_ntoa(addr.sin_addr),addr.sin_port,buff);  
+	       send(connfd,buff,n,0);
+       }
+
+       if(n<0)
+	       goto again;
 }
+int main(){
+	int listenfd,connfd,addrlen,pid;
+	struct sockaddr_in address;
+	if((listenfd = socket(AF_INET,SOCK_STREAM,0)) > 0)
+		printf("The socket was created\n");
+	else
+		printf("Error in Socket creation\n");
 
-int main() {
-    int cont, listenfd, connfd, addrlen, addrlen2, fd, pid, addrlen3;
-    struct sockaddr_in address, cli_address;
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port= htons(15001);
 
-    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) > 0) {
-        printf("The socket was created\n");
-    }
+	if( bind( listenfd,(struct sockaddr *)& address,sizeof(address)) ==0)
+		printf("Binding Socket\n");
+	else
+		printf("ERROR in binding\n");
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(15001);
-    printf("The address before bind %s...\n", inet_ntoa(address.sin_addr));
-    if (bind(listenfd, (struct sockaddr *)& address, sizeof(address)) == 0) {
-        printf("Binding Socket\n");
-    }
-    printf("The address after bind %s...\n", inet_ntoa(address.sin_addr));
+	if ((listen(listenfd, 3)) != 0){
+	      printf("Listen failed\n");
+	      exit(0);
+	}
+	else{
+		getsockname(listenfd,(struct sockaddr *) &address,&addrlen);
+		printf("Server listening on port  %d\n",address.sin_port);
+	}
 
-    listen(listenfd, 3);
-    printf("Server is listening\n");
-    getsockname(listenfd, (struct sockaddr *)&address, &addrlen3);
-    printf("The server's local address %s... and port %d\n", inet_ntoa(address.sin_addr), htons(address.sin_port));
-    while (1) {
-        addrlen = sizeof(struct sockaddr_in);
-        connfd = accept(listenfd, (struct sockaddr*)& cli_address, &addrlen);
-        addrlen2 = sizeof(struct sockaddr_in);
-        int i = getpeername(connfd, (struct sockaddr *)& cli_address, &addrlen);
-        printf("The Client  %s is connected on port %d\n", inet_ntoa(cli_address.sin_addr), htons(cli_address.sin_port));
 
-        if ((pid = fork()) == 0) {
-            printf("Inside child\n");
-            close(listenfd);
-            str_echo(connfd, htons(cli_address.sin_port));
-            exit(0);
-        }
-        close(connfd);
-    }
+	for(;;){
+		addrlen = sizeof(struct sockaddr_in);
+		connfd = accept(listenfd,(struct sockaddr *)& address,&addrlen);
+		printf(" connfd is %d ",connfd);
+		if(connfd>0)
+			printf("The client %s is connected \n", inet_ntoa(address.sin_addr));
+		else
+			printf("Not accepted\n");
 
-    return 0 ;
+		if((pid=fork())==0){
+			printf("Inside Child\n");
+                        close(listenfd);
+                        str_echo(connfd);
+                        exit(0);
+		}
+
+		close(connfd);
+	}
+
+	return 0;
 }
