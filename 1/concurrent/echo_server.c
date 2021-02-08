f79c9a1867cdff5ec47dcfcc7f8406c47546478a
Gracious Saxena <43841726+GraciousSaxena@users.noreply.github.com>
2021-02-08 19:45:15 +0530

    Update echo_server.c

--- 1/concurrent/echo_server.c
+++ 1/concurrent/echo_server.c
@@ -12,6 +12,76 @@
 #include<arpa/inet.h>
 
 void str_echo(int connfd){
+	int n, buffSize = 1024, addrlen;
+	char* buffer = malloc(buffSize);
+	struct sockaddr_in address;
+	
+	while((n = recv(connfd, buffer, buffSize, 0)) != 0) {
+		getpeername(connfd, (struct sockaddr*)&address, &addrlen);
+		printf("Client %s, connected at %d, sent - %s\n", inet_ntoa(address.sin_addr), address.sin_port, buffer);
+		send(connfd, buffer, n, 0);
+	}
+}
+
+int main(){
+	int pid, connfd, listenfd;
+	socklen_t addrlen;
+	struct sockaddr_in address;
+	
+	if((listenfd = socket(AF_INET, SOCK_STREAM, 0)) > 0){
+		printf("Socket Created!\n");
+	}
+	
+	address.sin_family = AF_INET;
+	address.sin_port = htons(15001);
+	address.sin_addr.s_addr = INADDR_ANY;
+	
+	addrlen = sizeof(address);
+	
+	if(bind(listenfd, (struct sockaddr*) &address, addrlen)){
+		printf("Bind Successful!\n");
+	}
+	
+	if(listen(listenfd, 3) == 0){
+		printf("Listening at port %d!\n", address.sin_port);
+	} else {
+		printf("Failed to listen");
+	}
+	
+	for(;;){
+		addrlen = sizeof(struct sockaddr_in);
+		connfd = accept(listenfd, (struct sockaddr*)&address, &addrlen);
+		if(connfd > 0)
+			printf("Client %s is connected!\n", inet_ntoa(address.sin_addr));
+		else
+			printf("Connection failed.\n");
+			
+		if((pid = fork()) == 0){
+			printf("Inside Child!\n");
+
+			close(listenfd);
+			str_echo(connfd);
+			exit(0);
+		}
+		close(connfd);
+	}
+	return 0;
+}
+
+/*#include<sys/types.h>
+#include<sys/socket.h>
+#include<sys/stat.h>
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+#include<netinet/in.h>
+#include<arpa/inet.h>
+
+void str_echo(int connfd){
 	int n,bufsize = 1024,len;
 	char *buff = malloc(bufsize);
 	struct sockaddr_in addr;
@@ -73,3 +143,4 @@ int main(){
 
 	return 0;
 }
+*/
