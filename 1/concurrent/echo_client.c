93174208444e9e100b2de22d84d150cdff8e6674
Gracious Saxena <43841726+GraciousSaxena@users.noreply.github.com>
2021-02-08 19:45:44 +0530

    Update echo_client.c

--- 1/concurrent/echo_client.c
+++ 1/concurrent/echo_client.c
@@ -1,4 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+
+#include<arpa/inet.h>
+#include<netinet/in.h>
+
+#include<sys/socket.h>
+#include<sys/stat.h>
 #include<sys/types.h>
+
+void str_echo(FILE *fp, int sockfd){
+	int buffSize = 1024;
+	char* buffer = malloc(buffSize);
+	
+	while(fgets(buffer, buffSize, fp) != NULL){
+		send(sockfd, buffer, buffSize, 0);
+		
+		if(recv(sockfd, buffer, buffSize, 0) >= 0){
+			fputs(buffer, stdout);
+		}
+	}
+}
+
+int main(int argc, char*argv[]){
+	int sockfd, conn;
+	socklen_t addrlen;
+	struct sockaddr_in address;
+	
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) > 0)
+		printf("Socket Created %d!\n", sockfd);
+		
+	address.sin_family = AF_INET;
+	address.sin_port = htons(15001);
+	
+	inet_pton(AF_INET, argv[1], &address.sin_addr.s_addr);
+	
+//	addrlen = sizeof(address);
+	if((conn = connect(sockfd, (struct sockaddr*)&address, sizeof(address))) == 0)
+		printf("Connected to Server!\n");
+	else 
+		printf("Connection failed!\n");
+
+	str_echo(stdin, sockfd);
+	
+	return 0;
+}
+
+/*#include<sys/types.h>
 #include<sys/socket.h>
 #include<sys/stat.h>
 
@@ -58,3 +107,4 @@ int main(int argc, char* argv[]){
 
 	return 0;
 }
+*/
